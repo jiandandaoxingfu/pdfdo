d6e26499649c0591cfb4431e06b463a2fc00330d
JMx <33108686+jiandandaoxingfu@users.noreply.github.com>
2022-06-28 11:51:30 +0800

    add word2pdf in gui

--- word2pdf.py
+++ word2pdf.py
@@ -14,12 +14,14 @@ class Word2PDF:
     def run(self):
         valueList = []
         try:
+            self.message = '开始转换, 请稍后'
             gencache.EnsureModule('{00020905-0000-0000-C000-000000000046}', 0, 8, 4)
             # 开始转换
             w = Dispatch("Word.Application")
-            for fullfilename in self.infn:
-                (filepath, filename) = os.path.split(fullfilename)  # 分割文件路径和文件名
-                softfilename = os.path.splitext(filename)  # 分割文件名和扩展名
+            for i in range(len(self.infn)):
+                filename = self.infn[i]
+                (filepath, filename) = os.path.split(filename)  
+                softfilename = os.path.splitext(filename) 
                 os.chdir(filepath)
                 doc = os.path.abspath(filename)
                 os.chdir(filepath)
@@ -32,20 +34,18 @@ class Word2PDF:
                     doc.ExportAsFixedFormat(output, constants.wdExportFormatPDF,
                                     Item=constants.wdExportDocumentWithMarkup,
                                     CreateBookmarks=constants.wdExportCreateHeadingBookmarks)
+                    self.message = str(i+1) + '/' + str(len(self.infn))
                 except Exception as e:
                     print(e)
                 if os.path.isfile(pdf_name):
                     valueList.append(pdf_name)
                 else:
-                    self.message = '转换失败！'
+                    self.message = '出错:'
                     return False
             w.Quit(constants.wdDoNotSaveChanges)
+            self.message = '完成'
             return valueList
         except TypeError as e:
-            self.message = '出错了！'
+            self.message = '出错'
             print(e)
             return False
-
-word2pdf = Word2PDF()
-word2pdf.infn = ["G:1.docx"];
-word2pdf.run();
